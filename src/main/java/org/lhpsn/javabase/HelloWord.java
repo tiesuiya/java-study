12ae16656d57693f5efd4d806675265356660967
tiesuiya <517303073@qq.com>
2018-03-23 11:09:44 +0800

    init

---
+++ src/main/java/org/lhpsn/javabase/HelloWord.java
@@ -0,0 +1,13 @@
+package org.lhpsn.javabase;
+
+/**
+ * Hello World
+ *
+ * @author lh
+ * @since 1.0.0
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
