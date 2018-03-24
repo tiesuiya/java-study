4933770ed2252d3d5eddf6a1a3c90e0adf914242
tiesuiya <517303073@qq.com>
2018-03-25 00:25:00 +0800

    sort

--- src/main/java/org/lhpsn/util/SortUtil.java
+++ src/main/java/org/lhpsn/util/SortUtil.java
@@ -1,6 +1,6 @@
 package org.lhpsn.util;
 
-import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * 排序帮助类
@@ -11,19 +11,16 @@ import java.util.Arrays;
 public final class SortUtil {
 
     /**
-     * 冒泡排序
+     * 高效冒泡排序
      *
      * @param list 待排序集合
      * @param <T>  待排序对象
      */
     public static <T extends Comparable<T>> void sort(T[] list) {
         boolean swapped = true;
-        for (int i = 1, len = list.length; i < len && swapped; ++i) {
-            System.out.println("外第" + i + "轮(本次需要比较" + (len - i) + "次)：");
+        for (int i = 1, len = list.length; i < len && swapped; i++) {
             swapped = false;
-            for (int j = 0; j < len - i; ++j) {
-                System.out.print(Arrays.asList(list));
-                System.out.print("\t内第" + (j + 1) + "轮：" + list[j] + "和" + list[j + 1] + "相比;\t\n");
+            for (int j = 0; j < len - i; j++) {
                 if (list[j].compareTo(list[j + 1]) > 0) {
                     T temp = list[j];
                     list[j] = list[j + 1];
@@ -34,17 +31,23 @@ public final class SortUtil {
         }
     }
 
-    public static void mySort(Integer[] array) {
-        int i, j, len = array.length;
-        for (i = 0; i < len - 1; i++) {
-            System.out.println("外第" + (i + 1) + "轮(本次需要比较" + len + "次)：");
-            for (j = i + 1; j < len; j++) {
-                System.out.print(Arrays.asList(array));
-                System.out.print("\t内第" + j + "轮：" + array[i] + "和" + array[j] + "相比;\t\n");
-                if (array[i] < array[j]) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+    /**
+     * 高效冒泡排序（自定义排序重载）
+     *
+     * @param list 待排序集合
+     * @param <T>  待排序对象
+     * @param comp 比较方式
+     */
+    public static <T> void sort(T[] list, Comparator<T> comp) {
+        boolean swapped = true;
+        for (int i = 1, len = list.length; i < len && swapped; i++) {
+            swapped = false;
+            for (int j = 0; j < len - i; j++) {
+                if (comp.compare(list[j], list[j + 1]) > 0) {
+                    T temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    swapped = true;
                 }
             }
         }
--- src/test/java/org/lhpsn/util/SortUtilTest.java
+++ src/test/java/org/lhpsn/util/SortUtilTest.java
@@ -1,38 +1,74 @@
 package org.lhpsn.util;
 
+import org.junit.Assert;
 import org.junit.Test;
+import sun.security.util.Length;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 
 /**
+ * 测试排序帮助类
+ *
  * @author lh
  * @since 1.0.0
  */
 public class SortUtilTest {
-    @Test
-    public void testMySort() throws Exception {
-        Integer[] arr = getTestIntegerArray();
-        Integer[] arr1 = CloneUtil.clone(arr);
-        Integer[] arr2 = CloneUtil.clone(arr);
 
-        SortUtil.mySort(arr2);
-        SortUtil.sort(arr1);
+    @Test
+    public void testSort1() throws Exception {
+        // 第一种方法
+        Integer[] arr = getTestIntegerArray();
+        SortUtil.sort(arr);
+        String flag = arr[0] > arr[1] ? "DESC" : "ASC";
+        for (int i = 0; i < arr.length; i++) {
+            if ("ASC".equals(flag)) {
+                if (i != arr.length - 1) {
+                    Assert.assertTrue(arr[i] < arr[i + 1]);
+                }
+            } else {
+                if (i != arr.length - 1) {
+                    Assert.assertTrue(arr[i] > arr[i + 1]);
+                }
+            }
+        }
     }
 
-    public Integer[] getTestIntegerArray() {
-        int len = 15;
+    public void testSort2() {
+        // 第二种方法
+        Integer[] arr = getTestIntegerArray();
+        SortUtil.sort(arr, new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        String flag = arr[0] > arr[1] ? "DESC" : "ASC";
+        for (int i = 0; i < arr.length; i++) {
+            if ("ASC".equals(flag)) {
+                if (i != arr.length - 1) {
+                    Assert.assertTrue(arr[i] < arr[i + 1]);
+                }
+            } else {
+                if (i != arr.length - 1) {
+                    Assert.assertTrue(arr[i] > arr[i + 1]);
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取size为100的随机数组
+     *
+     * @return 随机Integer数组
+     */
+    private Integer[] getTestIntegerArray() {
+        int len = 5;
         Integer[] arr = new Integer[len];
         for (int i = 0; i < len; i++) {
             arr[i] = new Random().nextInt();
         }
-        arr[len - 5] = 6;
-        arr[len - 4] = 6;
-        arr[len - 3] = 6;
-        arr[len - 2] = 6;
-        arr[len - 1] = 6;
-        System.out.println(Arrays.asList(arr));
         return arr;
     }
 }
