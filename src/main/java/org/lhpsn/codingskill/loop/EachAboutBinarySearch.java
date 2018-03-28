f153f115d933634da39db377f55d816cb55dc841
tiesuiya <517303073@qq.com>
2018-03-28 09:00:07 +0800

    binarySearch

--- src/main/java/org/lhpsn/codingskill/loop/EachAboutBinarySearch.java
+++ src/main/java/org/lhpsn/codingskill/loop/EachAboutBinarySearch.java
@@ -1,7 +1,5 @@
 package org.lhpsn.codingskill.loop;
 
-import java.util.Arrays;
-
 /**
  * 关于二分查找的循环实现
  *
@@ -13,31 +11,64 @@ public class EachAboutBinarySearch {
     public static void main(String[] args) {
         EachAboutBinarySearch eachBinarySearch = new EachAboutBinarySearch();
         int[] ints = new int[]{1, 2, 3, 4, 5};
-        System.out.println(eachBinarySearch.binarySearch(ints, 6));
-        ints = new int[]{1,3};
-        System.out.println(eachBinarySearch.binarySearch(ints, 6));
+        System.out.println(eachBinarySearch.binarySearch1(ints, 6));
+        ints = new int[]{1, 3};
+        System.out.println(eachBinarySearch.binarySearch1(ints, 6));
         ints = new int[]{1};
-        System.out.println(eachBinarySearch.binarySearch(ints, 0));
+        System.out.println(eachBinarySearch.binarySearch1(ints, 0));
         ints = new int[]{1, 2, 3, 4, 5};
-        System.out.println(eachBinarySearch.binarySearch(ints, 0));
+        System.out.println(eachBinarySearch.binarySearch1(ints, 0));
         ints = new int[]{1, 2, 3, 4, 5};
-        System.out.println(eachBinarySearch.binarySearch(ints, 1));
+        System.out.println(eachBinarySearch.binarySearch1(ints, 1));
         ints = new int[]{1, 2, 3, 4, 5};
-        System.out.println(eachBinarySearch.binarySearch(ints, 5));
-        ints = new int[]{};
-        System.out.println(eachBinarySearch.binarySearch(ints, 0));
-        ints = new int[]{};
-        System.out.println(eachBinarySearch.binarySearch(ints, 10));
+        System.out.println(eachBinarySearch.binarySearch1(ints, 5));
+        System.out.println(".....................................");
+        ints = new int[]{1, 2, 3, 4, 5};
+        System.out.println(eachBinarySearch.binarySearch1(ints, -1));
+        ints = new int[]{10, 20, 30, 40, 50};
+        System.out.println(eachBinarySearch.binarySearch1(ints, 25));
+        ints = new int[]{10, 20, 30, 40, 50};
+        System.out.println(eachBinarySearch.binarySearch1(ints, 55));
+        ints = new int[]{1, 2};
+        System.out.println(eachBinarySearch.binarySearch1(ints, 1));
+        ints = new int[]{1, 2};
+        System.out.println(eachBinarySearch.binarySearch1(ints, 2));
     }
+
 
     /**
      * 二分查找的循环实现
+     *
+     * @param arr 需要查找的数组，必须是一个sorted数组
+     * @param k   需要查找值
+     * @return 值所在数组位置
+     */
+    public int binarySearch1(int[] arr, int k) {
+        // 这里使用arr.length原因：[a,b) 开闭区间具有查找优势:
+        // [a,m) + [m,b) = [a,b)
+        int a = 0;
+        int b = arr.length;
+        while (b > a) {
+            int m = a / 2 + b / 2;
+            if (k > arr[m]) {
+                a = m + 1;
+            } else if (k < arr[m]) {
+                b = m;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 二分查找的循环实现(自己实现的)
      *
      * @param arrays     需要查找的数组，必须是一个sorted数组
      * @param searchData 需要查找值
      * @return 值所在数组位置
      */
-    public int binarySearch(int[] arrays, int searchData) {
+    public int binarySearchDiy(int[] arrays, int searchData) {
 
         if (arrays == null || arrays.length == 0) {
             return -1;
@@ -72,8 +103,7 @@ public class EachAboutBinarySearch {
             } else if (mValue < searchData) {
                 a = m;
             } else {
-                index = m;
-                break;
+                return m;
             }
 
         }
