52abcf86863fd5f77fa753e3de830a80328b3b1c
tiesuiya <517303073@qq.com>
2018-04-02 09:42:43 +0800

    decorator pattern & singleton pattern

--- src/main/java/org/lhpsn/javabase/designpattern/Singleton.java
+++ src/main/java/org/lhpsn/javabase/designpattern/Singleton.java
@@ -1,8 +1,54 @@
 package org.lhpsn.javabase.designpattern;
 
 /**
+ * Singleton Pattern
+ *
  * @author lh
  * @since 1.0.0
  */
 public class Singleton {
+
+    public static void main(String[] args) {
+        SingletonLazy singletonLazy1 = SingletonLazy.getInstance();
+        SingletonLazy singletonLazy2 = SingletonLazy.getInstance();
+        SingletonHungry singletonHungry1 = SingletonHungry.getInstance();
+        SingletonHungry singletonHungry2 = SingletonHungry.getInstance();
+        System.out.println(singletonLazy1);
+        System.out.println(singletonLazy2);
+        System.out.println(singletonHungry1);
+        System.out.println(singletonHungry2);
+    }
+}
+
+/**
+ * lazy pattern
+ */
+class SingletonLazy {
+
+    private static SingletonLazy instance = new SingletonLazy();
+
+    private SingletonLazy() {
+    }
+
+    public static SingletonLazy getInstance() {
+        return instance;
+    }
+}
+
+/**
+ * hungry pattern
+ */
+class SingletonHungry {
+
+    private static SingletonHungry instance = null;
+
+    private SingletonHungry() {
+    }
+
+    public synchronized static SingletonHungry getInstance() {
+        if (instance == null) {
+            instance = new SingletonHungry();
+        }
+        return instance;
+    }
 }
